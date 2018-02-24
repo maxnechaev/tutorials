3dc7c01414d6e6ffcea61d3ae21b8def22ac1cc5
maxnechaev <4max.nechaev@gmail.com>
2018-02-24 18:32:36 +0400

    Delete urls.py

--- django-examples/mysite/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include ('mainapp.urls')),
-    url(r'^news/', include ('news.urls')),
-]
